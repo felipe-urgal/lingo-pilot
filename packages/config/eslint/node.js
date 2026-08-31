16b0caf45b5f82c24035089c8bee74cbbdee49e8
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-08-31 15:27:51 -0300

    refactor: expose reusable node eslint environment

--- packages/config/eslint/node.js
+++ packages/config/eslint/node.js
@@ -1,11 +1,10 @@
 import globals from "globals";
 import { baseConfig } from "./base.js";
 
-export const nodeJsConfig = [
-  ...baseConfig,
-  {
-    languageOptions: {
-      globals: globals.node,
-    },
+export const nodeEnvironmentConfig = {
+  languageOptions: {
+    globals: globals.node,
   },
-];
+};
+
+export const nodeJsConfig = [...baseConfig, nodeEnvironmentConfig];
