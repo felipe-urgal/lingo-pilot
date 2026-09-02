204ea9ce82abcace36dc461f4ae6ef032aa24bf9
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-09-02 16:37:16 -0300

    refactor: decouple telemetry from server config

--- apps/web/server/observability/runtime.ts
+++ apps/web/server/observability/runtime.ts
@@ -1,14 +1,14 @@
-import { deploymentMetadata, serverConfig } from "../../config/server";
+import { observabilityConfig } from "../../config/observability";
 import { noopTelemetryHooks } from "./contracts";
 import { createLogger } from "./logger";
 import { createRequestObserver } from "./request";
 
 export const serverLogger = createLogger({
   service: "lingo-pilot-web",
-  environment: serverConfig.profile,
-  version: deploymentMetadata.version,
-  deploymentId: deploymentMetadata.deploymentId,
-  pretty: serverConfig.profile !== "production",
+  environment: observabilityConfig.environment,
+  version: observabilityConfig.version,
+  deploymentId: observabilityConfig.deploymentId,
+  pretty: observabilityConfig.pretty,
 });
 
 export const observeRequest = createRequestObserver(
